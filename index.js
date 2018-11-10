9f463bc1217906cef12df42c486f3294ad99e67d
brianemilius <brian@brianemilius.com>
2018-11-10 17:01:29 +0100

    fix paths for log file

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = function (options = {
-	'path': 'logs',
+	'path': './logs',
 	'logName': 'errors.log'
 }) {
-	if (!fs.existsSync(path.join(__dirname, options.path)))
-		fs.mkdirSync(path.join(__dirname, options.path));
+	if (!fs.existsSync(options.path))
+		fs.mkdirSync(options.path);
 	return function (err, req, res, next) {
 		if (err) {
-			const fullPath = path.join(__dirname, options.path, options.logName);
+			const fullPath = path.join(options.path, options.logName);
 			if (!fs.existsSync(fullPath))
 				fs.writeFileSync(fullPath, '');
 			const oldLog = fs.readFileSync(fullPath);
